56f37597a54f1c1dae72c79c036be0e15b794fbb
Prasanna13882 <141567760+Prasanna13882@users.noreply.github.com>
2025-02-05 10:11:20 +0530

    updated monitor BFM and proxy

--- src/hdlTop/slaveAgentBFM/AhbSlaveMonitorBFM.sv
+++ src/hdlTop/slaveAgentBFM/AhbSlaveMonitorBFM.sv
@@ -1,71 +1,44 @@
  `ifndef AHBSLAVEMONITORBFM_INCLUDED_
 `define AHBSLAVEMONITORBFM_INCLUDED_
 
-//-------------------------------------------------------
-// Importing apb global package
-//-------------------------------------------------------
 import AhbGlobalPackage::*;
 
-//--------------------------------------------------------------------------------------------
-// Inteface: AhbSlaveMonitorBFM
-//  Connects the slave monitor bfm with the monitor proxy 
-//  to call the tasks and functions from apb monitor bfm to apb monitor proxy
-//--------------------------------------------------------------------------------------------
 interface AhbSlaveMonitorBFM (input  bit   hclk,
                               input  bit  hresetn,
-                              input logic [ADDR_WIDTH-1:0] haddr,
-                              input logic [2:0] hburst,
-                              input logic hmastlock,
+						      input logic [2:0] hburst,
+ 						      input logic hmastlock,
+                              input logic [ADDR_WIDTH-1:0] haddr,                                                        
                               input logic [HPROT_WIDTH-1:0] hprot,
                               input logic [2:0] hsize,
                               input logic hnonsec,
                               input logic hexcl,
                               input logic [HMASTER_WIDTH-1:0] hmaster,
-                              input logic [1:0] htrans,     
-                              input logic hwrite,
-                              input logic [DATA_WIDTH-1:0] hwdata,
-                              input logic [(DATA_WIDTH/8)-1:0]hwstrb,
-                              output logic [DATA_WIDTH-1:0] hrdata,
-                              output logic hready,
-                              output logic hreadyout,
-                              output logic hresp,
-                              output logic hexokay,
+                              input logic [1:0] htrans, 
+			     			  input logic [DATA_WIDTH-1:0] hwdata,
+                              input logic [(DATA_WIDTH/8)-1:0]hwstrb,    
+                              input logic hwrite,                              
+                              input logic [DATA_WIDTH-1:0] hrdata,
+			     			  input logic hreadyout,
+                              input logic hresp,
+                              input logic hexokay,
+                              input logic hready,                             
                               input logic [NO_OF_SLAVES-1:0]hselx
                                );
 
-  //-------------------------------------------------------
-  // Importing uvm package
-  //-------------------------------------------------------
+
   import uvm_pkg::*;
   `include "uvm_macros.svh"
-  
-  //-------------------------------------------------------
-  // Importing slave driver proxy
-  //------------------------------------------------------- 
+
   import AhbSlavePackage::*;
-  
-  //Variable : name
-  //Used to store the name of the interface
+
   string name = "AHB_SLAVE_MONITOR_BFM";
   
-
-  //Variable: ahb_slave_drv_proxy_h
-  //Declaring handle for AhbSlaveDriverProxy
-  
   AhbSlaveMonitorProxy ahbSlaveMonitorProxy;
-  
 
-  //-------------------------------------------------------
-  // Used to display the name of the interface
-  //-------------------------------------------------------
   initial begin
     `uvm_info(name,$sformatf(name),UVM_LOW);
   end
 
-  //-------------------------------------------------------
-  // Task: waitForResetn
-  //  Waiting for the system reset to be active low
-  //-------------------------------------------------------
   task waitForResetn();
    @(negedge hresetn);
     `uvm_info(name, $sformatf("SYSTEM_RESET_DETECTED"), UVM_HIGH)
@@ -74,18 +47,31 @@ interface AhbSlaveMonitorBFM (input  bit   hclk,
     `uvm_info(name, $sformatf("SYSTEM_RESET_DEACTIVATED"), UVM_HIGH)
   endtask : waitForResetn
 
-  //-------------------------------------------------------
-  // Task: sampleData
-  //  In this task, the pwdata and prdata is sampled
-  //
-  // Parameters: 
-  //  ahbDataPacket - Handle for ahbTransferCharStruct class 
-  //  ahbConfigPacket  - Handle for ahbTransferCharStruct class
-  //-------------------------------------------------------
- task sampleData (output ahbTransferCharStruct ahbDataPacket, input ahbTransferConfigStruct ahbConfigPacket);
-    //logic to be written
-  endtask : sampleData
+ task slaveSampleData (output ahbTransferCharStruct ahbDataPacket, input ahbTransferConfigStruct ahbConfigPacket);
+    @(posedge hclk);
+    while( hready !=1 && hresp==1) begin
+      `uvm_info(name, $sformatf("Inside while loop HREADY"), UVM_HIGH)
+      @(posedge hclk);
+    end   
+    ahbDataPacket.hselx  = hselx;
+    ahbDataPacket.haddr  = haddr;
+    ahbDataPacket.hburst = ahbBurstEnum'(hburst);
+    ahbDataPacket.hwrite = hwrite;
+    ahbDataPacket.hsize = ahbHsizeEnum'(hsize);
+    ahbDataPacket.htrans = ahbTransferEnum'(htrans);
+    ahbDataPacket.hnonsec = hnonsec;
+    ahbDataPacket.hprot = ahbProtectionEnum'(hprot);	
+    ahbDataPacket.hresp = ahbRespEnum'(hresp);
+    
+    if(!hwrite) begin
+      ahbDataPacket.hwdata = hwdata;
+      ahbDataPacket.hwstrb  = hwstrb;
+    end
+    else
+      ahbDataPacket.hrdata = hrdata;
+  endtask : slaveSampleData
 
 endinterface : AhbSlaveMonitorBFM
 
 `endif
+
--- src/hvlTop/slaveAgent/AhbSlaveMonitorProxy.sv
+++ src/hvlTop/slaveAgent/AhbSlaveMonitorProxy.sv
@@ -1,29 +1,15 @@
 `ifndef AHBSLAVEMONITORPROXY_INCLUDED_
 `define AHBSLAVEMONITORPROXY_INCLUDED_
 
-//--------------------------------------------------------------------------------------------
-// Class: AhbSlaveMonitorProxy
-//  This is the HVL side AhbSlaveMonitorProxy
-//  It gets the sampled data from the HDL slave monitor and converts them into transaction items
-//--------------------------------------------------------------------------------------------
 class AhbSlaveMonitorProxy extends uvm_monitor; 
   `uvm_component_utils(AhbSlaveMonitorProxy)
-  
-  // Variable: ahbSlaveMonitorBFM
-  // Declaring handle for ahb monitor bfm
+
   virtual AhbSlaveMonitorBFM ahbSlaveMonitorBFM;
-   
-  // Variable: ahbSlaveAgentConfig
-  // Declaring handle for AhbSlaveAgentConfig class 
+
   AhbSlaveAgentConfig ahbSlaveAgentConfig;
-    
-  // Variable: ahbSlaveAnalysisPort
-  // Declaring analysis port for the monitor port
+
   uvm_analysis_port#(AhbSlaveTransaction) ahbSlaveAnalysisPort;
-  
-  //-------------------------------------------------------
-  // Externally defined Tasks and Functions
-  //-------------------------------------------------------
+
   extern function new(string name = "AhbSlaveMonitorProxy", uvm_component parent);
   extern virtual function void build_phase(uvm_phase phase);
   extern virtual function void end_of_elaboration_phase(uvm_phase phase);
@@ -31,26 +17,11 @@ class AhbSlaveMonitorProxy extends uvm_monitor;
 
 endclass : AhbSlaveMonitorProxy
 
-//--------------------------------------------------------------------------------------------
-// Construct: new
-//  Initializes memory for new object
-//
-// Parameters:
-//  name   - AhbSlaveMonitorProxy
-//  parent - parent under which this component is created
-//--------------------------------------------------------------------------------------------
 function AhbSlaveMonitorProxy::new(string name = "AhbSlaveMonitorProxy",uvm_component parent);
   super.new(name, parent);
   ahbSlaveAnalysisPort = new("ahbSlaveAnalysisPort",this);
 endfunction : new
 
-//--------------------------------------------------------------------------------------------
-// Function: build_phase
-//  Creates the required ports, gets the required configuration from confif_db
-//
-// Parameters:
-//  phase - uvm phase
-//--------------------------------------------------------------------------------------------
 function void AhbSlaveMonitorProxy::build_phase(uvm_phase phase);
   super.build_phase(phase);
   if(!uvm_config_db #(virtual AhbSlaveMonitorBFM)::get(this,"","AhbSlaveMonitorBFM", ahbSlaveMonitorBFM)) begin
@@ -58,26 +29,11 @@ function void AhbSlaveMonitorProxy::build_phase(uvm_phase phase);
   end
 endfunction : build_phase
 
-//--------------------------------------------------------------------------------------------
-// Function: end_of_elaboration_phase
-//  Pointing handle of monitor proxy in HDL BFM to this proxy method in HVL part
-//
-// Parameters:
-//  phase - uvm phase
-//--------------------------------------------------------------------------------------------
 function void AhbSlaveMonitorProxy::end_of_elaboration_phase(uvm_phase phase);
   super.end_of_elaboration_phase(phase);
   ahbSlaveMonitorBFM.ahbSlaveMonitorProxy = this;
 endfunction : end_of_elaboration_phase
 
-//--------------------------------------------------------------------------------------------
-// Task: run_phase
-//  This task calls the monitor logic written in the monitor BFM at HDL side
-//  Receives data packet from slave monitor bfm and converts into the transaction objects
-//
-// Parameters:
-//  phase - uvm phase
-//--------------------------------------------------------------------------------------------
 task AhbSlaveMonitorProxy::run_phase(uvm_phase phase);
   AhbSlaveTransaction ahbSlavePacket;
 
@@ -86,13 +42,13 @@ task AhbSlaveMonitorProxy::run_phase(uvm_phase phase);
   
   ahbSlaveMonitorBFM.waitForResetn();
 
- /* forever begin
+  forever begin
     ahbTransferCharStruct structDataPacket;
     ahbTransferConfigStruct  structConfigPacket; 
     AhbSlaveTransaction  ahbSlaveClonePacket;
     
     AhbSlaveConfigConverter :: fromClass (ahbSlaveAgentConfig, structConfigPacket);
-    ahbSlaveMonitorBFM.sampleData (structDataPacket, structConfigPacket);
+    ahbSlaveMonitorBFM.slaveSampleData (structDataPacket, structConfigPacket);
     AhbSlaveSequenceItemConverter :: toClass (structDataPacket, ahbSlavePacket);
 
     `uvm_info(get_type_name(),$sformatf("Received packet from slave monitor BFM: , \n %s", ahbSlavePacket.sprint()),UVM_HIGH)
@@ -102,7 +58,7 @@ task AhbSlaveMonitorProxy::run_phase(uvm_phase phase);
     `uvm_info(get_type_name(),$sformatf("Sending packet via analysis port: , \n %s", ahbSlaveClonePacket.sprint()),UVM_HIGH)
     ahbSlaveAnalysisPort.write(ahbSlaveClonePacket);
   end
-*/
+
 endtask : run_phase
 
 `endif

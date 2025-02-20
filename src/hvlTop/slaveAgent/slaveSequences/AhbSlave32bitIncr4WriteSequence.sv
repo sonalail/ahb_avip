7349668fcf9dbd7c0e30514a3848c7cbe92c33bb
prakhyath06 <159763985+prakhyath06@users.noreply.github.com>
2025-02-20 10:02:26 +0530

    wrote Incr4 sequence and test

---
+++ src/hvlTop/slaveAgent/slaveSequences/AhbSlave32bitIncr4WriteSequence.sv
@@ -0,0 +1,25 @@
+`ifndef AHBSLAVE32BITINCR4WRITESEQUENCE_INCLUDED_
+`define AHBSLAVE32BITINCR4WRITESEQUENCE_INCLUDED_  
+
+class AhbSlave32bitIncr4WriteSequence extends AhbSlaveBaseSequence;
+  `uvm_object_utils(AhbSlave32bitIncr4WriteSequence)
+  extern function new(string name="AhbSlave32bitIncr4WriteSequence");
+  extern task body();
+endclass : AhbSlave32bitIncr4WriteSequence
+ 
+function AhbSlave32bitIncr4WriteSequence::new(string name="AhbSlave32bitIncr4WriteSequence");
+  super.new(name);
+endfunction : new
+ 
+task AhbSlave32bitIncr4WriteSequence::body();
+  req = AhbSlaveTransaction::type_id::create("req");
+  start_item(req);
+  if(!req.randomize(){
+		      noOfWaitStates == 32'h0000;
+   		     }) begin
+    `uvm_fatal("APB","Rand failed");
+  end
+  req.print();
+  finish_item(req);
+endtask : body
+`endif

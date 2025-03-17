a41e3543c5593d5519b03d0539fbb2f3ba1eff09
nishashetty <109810083+nishashetty11@users.noreply.github.com>
2025-03-17 15:53:10 +0530

    Removed comments in slaveagent

--- src/hvlTop/slaveAgent/AhbSlaveCoverage.sv
+++ src/hvlTop/slaveAgent/AhbSlaveCoverage.sv
@@ -20,28 +20,6 @@ class AhbSlaveCoverage extends uvm_subscriber #(AhbSlaveTransaction);
       bins ahbResp0={0};
       ignore_bins ahbResp1={1};
     }
-
-/*     HRDATA_CP:coverpoint ahbSlaveTransaction.hrdata{
-      option.comment="ahb read data";
-       bins ahbRdata1       ={0};
-       bins ahbRdata2       ={1};
-       bins ahbRdata4       ={3};
-       bins ahbRdata8       ={7};
-       bins ahbRdata16		={15};
-       bins ahbRdata32		={31};
-       bins ahbRdata64		={63};
-       bins ahbRdata128	    ={127};
-       bins ahbRdata256	    ={255};
-       bins ahbRdataDefault =default;
-    }*/
-
-    /* HEXOKAY_CP:coverpoint ahbSlaveTransaction.hexokay{
-      option.comment="ahb exclusive okay";
-       bins ahbExokay0={0};
-       bins ahbExokay1={1};
-    }*/
-    
-   // HREADY_CP_x_HRDATA_CP_x_HRESP_CP:cross HREADY_CP,HRDATA_CP,HRESP_CP;
   
   endgroup: ahbSlaveCovergroup
 
@@ -57,15 +35,17 @@ function  AhbSlaveCoverage::new(string name = "AhbSlaveCoverage", uvm_component 
 endfunction : new
 
 function void AhbSlaveCoverage::write(AhbSlaveTransaction t);
-  
+
   `uvm_info(get_type_name(),"AHB SLAVE COVERAGE",UVM_LOW);
   ahbSlaveCovergroup .sample(ahbSlaveAgentConfig,t);
-  
+
 endfunction : write
 
 function void AhbSlaveCoverage::report_phase(uvm_phase phase);
+      
   `uvm_info(get_type_name(), $sformatf("Entered the report_phase of AHB Slave Coverage"), UVM_NONE);
   `uvm_info(get_type_name(), $sformatf("AHB Slave Agent Coverage = %0.2f %%", ahbSlaveCovergroup.get_coverage()), UVM_NONE);
+      
 endfunction: report_phase
 
 `endif
--- src/hvlTop/slaveAgent/AhbSlaveDriverProxy.sv
+++ src/hvlTop/slaveAgent/AhbSlaveDriverProxy.sv
@@ -10,13 +10,11 @@ class AhbSlaveDriverProxy extends uvm_driver#(AhbSlaveTransaction);
 
   AhbSlaveAgentConfig ahbSlaveAgentConfig;
 
-
   extern function new(string name = "AhbSlaveDriverProxy", uvm_component parent = null);
   extern virtual function void build_phase(uvm_phase phase);
   extern virtual function void connect_phase(uvm_phase phase);
   extern function void end_of_elaboration_phase(uvm_phase phase);
   extern virtual task run_phase(uvm_phase phase);
-  //extern virtual task checkForHresp(inout ahbTransferCharStruct structPacket );
   extern virtual task taskWrite(inout ahbTransferCharStruct structPacket);
   extern virtual task taskRead(inout ahbTransferCharStruct structPacket);
 
@@ -52,50 +50,34 @@ task AhbSlaveDriverProxy::run_phase(uvm_phase phase);
   forever begin
     ahbTransferCharStruct structPacket;
     ahbTransferConfigStruct structConfig;
-    
-  //  checkForHresp(structPacket);
-  //  `uvm_info("DEBUG_NA", $sformatf("AFTER HRESP_CHECK_5 -struct:: %p", structPacket), UVM_MEDIUM); 
-//    `uvm_info(get_type_name(), "Waiting for a transaction...", UVM_LOW)
+   
     seq_item_port.get_next_item(req);
-  //  `uvm_info(get_type_name(), "Transaction received!", UVM_LOW)
-
-    //`uvm_info(get_type_name(), $sformatf("REQ-SLAVE_TX \n %s",req.sprint),UVM_LOW);
-  if(req.choosePacketData) begin  
+    if(req.choosePacketData) begin  
 
     AhbSlaveSequenceItemConverter::fromClass(req, structPacket); 
-   // `uvm_info(get_type_name(), $sformatf("Converted structPacket: %p", structPacket), UVM_HIGH)
-
     AhbSlaveConfigConverter::fromClass(ahbSlaveAgentConfig, structConfig);
-  //  `uvm_info(get_type_name(), $sformatf("Converted structConfig: %p", structConfig), UVM_HIGH)
 
-
-     ahbSlaveDriverBFM.slaveDriveToBFM(structPacket, structConfig);
+    ahbSlaveDriverBFM.slaveDriveToBFM(structPacket, structConfig);
 
     AhbSlaveSequenceItemConverter::toClass(structPacket, req);  
 
     `uvm_info("SONAL", $sformatf("STRUCTPACKET = %p",req), UVM_LOW)
-if(structPacket.hwrite == WRITE)begin
-    
-    `uvm_info("AIL", "ENTERED WRITE LOOP", UVM_LOW)
-      taskWrite(structPacket);
+      
+      if(structPacket.hwrite == WRITE)begin   
+        `uvm_info("AIL", "ENTERED WRITE LOOP", UVM_LOW)
+        taskWrite(structPacket);
+      end
+      else begin
+        taskRead(structPacket);
+      end
     end
-else begin
-      taskRead(structPacket);
-end
-end
-else
- begin
-
- AhbSlaveSequenceItemConverter::fromClass(req, structPacket); 
-   // `uvm_info(get_type_name(), $sformatf("Converted structPacket: %p", structPacket), UVM_HIGH)
-
-    AhbSlaveConfigConverter::fromClass(ahbSlaveAgentConfig, structConfig);
-  //  `uvm_info(get_type_name(), $sformatf("Converted structConfig: %p", structConfig), UVM_HIGH)
-
-     ahbSlaveDriverBFM.slaveDriveToBFM(structPacket, structConfig);
-
-    AhbSlaveSequenceItemConverter::toClass(structPacket, req); 
-end
+    else
+      begin
+        AhbSlaveSequenceItemConverter::fromClass(req, structPacket); 
+        AhbSlaveConfigConverter::fromClass(ahbSlaveAgentConfig, structConfig);
+        ahbSlaveDriverBFM.slaveDriveToBFM(structPacket, structConfig);
+        AhbSlaveSequenceItemConverter::toClass(structPacket, req); 
+      end
     seq_item_port.item_done();
   end
 endtask : run_phase
@@ -109,59 +91,28 @@ task AhbSlaveDriverProxy::taskWrite(inout ahbTransferCharStruct structPacket);
     
     if(structPacket.hwstrb[i] == 1) begin
       ahbSlaveAgentConfig.slaveMemoryTask(structPacket.haddr+i,structPacket.hwdata[8*i+7 -: 8]);
-      `uvm_info("DEBUG_NA", $sformatf("task_write inside for loop data = %0h", 
-                                      ahbSlaveAgentConfig.slaveMemory[structPacket.haddr+i]), UVM_HIGH);
+      `uvm_info("DEBUG_NA", $sformatf("task_write inside for loop data = %0h",ahbSlaveAgentConfig.slaveMemory[structPacket.haddr+i]), UVM_HIGH);
     end
   end
 endtask : taskWrite
 
 task AhbSlaveDriverProxy::taskRead(inout ahbTransferCharStruct structPacket);
-bit memoryExist;
+  bit memoryExist;
 
   `uvm_info("DEBUG_NA", $sformatf("task_read"), UVM_HIGH);
   
-
   for(int i=0; i<(DATA_WIDTH/8); i++) begin
     if(ahbSlaveAgentConfig.slaveMemory.exists(structPacket.haddr)) begin
       structPacket.hrdata[8*i+7 -: 8] = ahbSlaveAgentConfig.slaveMemory[structPacket.haddr + i];
       memoryExist = 1;
     end
-    end
-
-    if(memoryExist == 0) begin
-             
-       `uvm_error(get_type_name(), $sformatf("Selected address has no data"));
+  end
+  if(memoryExist == 0) begin
+    `uvm_error(get_type_name(), $sformatf("Selected address has no data"));
       structPacket.hresp  = ERROR;
       structPacket.hrdata  = 'h0;
-    end
-
+  end
 endtask : taskRead
     
-/*
-task AhbSlaveDriverProxy:: checkForHresp(inout ahbTransferCharStruct structPacket);
-  
-  `uvm_info("DEBUG_NA", $sformatf("AFTER HRESP_CHECK_1 struct :: %p", structPacket), UVM_HIGH);
-  if(structPacket.haddr inside {[ ahbSlaveAgentConfig.minimumAddress: ahbSlaveAgentConfig.maximumAddress]}) begin
-    structPacket.hresp = OKAY;
-    
-    if(structPacket.hwrite==1) begin
-      taskWrite(structPacket);
-    end
-    else begin
-      taskRead(structPacket);
-    end
-  end
-  else begin
-   structPacket.hresp  = ERROR;
-   structPacket.hrdata  = 'h0;
-  end
-  
-  `uvm_info("DEBUG_NA", $sformatf("AFTER HRESP_CHECK_2 minimumAddress = %0h, maximumAddress=%0h ",
-                                  ahbSlaveAgentConfig.minimumAddress, ahbSlaveAgentConfig.maximumAddress), UVM_HIGH);
 
-  `uvm_info("DEBUG_NA", $sformatf("AFTER HRESP_CHECK_3 struct-Haddr :: %0h", structPacket.haddr), UVM_HIGH);
-  `uvm_info("DEBUG_NA-hresp", $sformatf("AFTER HRESP_CHECK_4 struct :: %p", structPacket), UVM_HIGH);
-
-endtask : checkForHresp
-*/
 `endif
